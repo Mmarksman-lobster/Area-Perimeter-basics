b4674e753f4cea68664d15bebd4989ffd1fe366c
Mmarksman-lobster <macmenigalll50218@masseyhigh.school.nz>
2026-06-19 14:55:23 +1200

    basic vairables

---
+++ p_02_Varivable.py
@@ -0,0 +1,11 @@
+# variables can be a string
+Greeting = "hello world"
+#variables can be a number
+fav_number = 13
+#variables can be boolean true false
+Love_programming = True
+
+num_string = "5"
+print(fav_number * 2)
+print(num_string * 5)
+PI = 3.14
